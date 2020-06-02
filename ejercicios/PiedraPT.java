b632ed3cd1b05a91a04a5a117949722baba5a38d
matahj <matahj@gmail.com>
2020-06-02 16:39:11 -0500

    piedra, papel o tijeras

---
+++ ejercicios/PiedraPT.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class PiedraPT {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String usrResp, cpuResp;
+        String ganador;
+
+        System.out.print("piedra, papel o tijeras: ");
+        usrResp = sc.nextLine();
+        System.out.println("Respuesta del USR: " + usrResp);
+
+        cpuResp = respuestaCPU();
+
+        System.out.println("Respuesta del CPU: " + cpuResp);
+
+        ganador = evalua(usrResp, cpuResp);
+
+        System.out.println("Ganador: " + ganador);
+
+    }
+
+    public static String evalua(String a, String b){
+        String r = "aunSinGanador";
+
+            
+
+
+        return r;
+    }
+
+    public static String respuestaCPU(){
+        String r = "aunSinVAlor";
+        double x;
+
+        x=Math.random();
+
+        if( x>0 && x<0.33 ){
+            r = "piedra";
+        }
+
+        if( x>0.33 && x<0.66){
+            r="papel";
+        }
+
+        if( x>0.66 && x<1.0 ){
+            r="tijeras";
+        }
+
+        return r;
+    }
+}
